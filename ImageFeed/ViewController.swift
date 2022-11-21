b44c341ac2e74c1bd35a792e8c9cb1378edd8965
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-11-21 21:50:51 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Екатерина Иванова on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
