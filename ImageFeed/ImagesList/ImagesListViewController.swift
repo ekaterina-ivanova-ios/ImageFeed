1dfb6ba03270a40e41dd702b928927d09be22573
Ekaterina <katiai@yandex.ru>
2022-11-22 20:29:14 +0300

    no message

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private var photosName = [String]()
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photosName = Array(0..<20).map{ "\($0)" } //создаёт массив чисел от 0 до 19 и возвращает массив строк
+    }
+    
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reusedIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "noActive")
+        cell.likeButton.setImage(likeImage, for: .normal)
+        
+    }
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    
+    
+}
