ed8de1eab5645c1c7fb08fe4e6b224b03061ce19
Ekaterina <katiai@yandex.ru>
2022-12-29 11:36:45 +0300

    Добавлены данные для взаимодействия с Unsplash

---
+++ ImageFeed/Keys/Constants.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class Constants {
+    let AccessKey = "PynczGdfM1b5gJokKViG1hqfVPZFqH5lmKsYBF-VoZY"
+    let SecretKey = "kjJylFBGKr5bYPs_1QBuat2PA-LedB-7zSgtCqkTroM"
+    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let AccessScope = "public+read_user+write_likes"
+    let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
+
