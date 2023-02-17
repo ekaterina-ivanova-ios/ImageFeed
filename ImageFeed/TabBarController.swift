abc5454f54875085e83dc78e5372df33ddf17ce4
Ekaterina <katiai@yandex.ru>
2023-02-17 10:53:23 +0300

    11 sprint - in_progress

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -1,15 +1,16 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
-        
-        let imageListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
-        let profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileViewController")
-    
-        self.viewControllers = [imageListViewController, profileViewController]
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+            let profileViewController = ProfileViewController()
+            profileViewController.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Profile", comment: ""),
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+        self.viewControllers = [imagesListViewController, profileViewController]
     }
-    
-}
+} 
